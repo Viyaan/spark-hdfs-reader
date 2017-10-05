0f408e85b52a78071d5ef861e7477204a7fa2145
Viyaan <viyaan.86@gmail.com>
2017-10-05 22:11:14 +0530

    updating spark code

--- src/main/scala/com/retriver/spark/HDFSFileReader.scala
+++ src/main/scala/com/retriver/spark/HDFSFileReader.scala
@@ -1,3 +1,5 @@
+package com.retriver.spark
+
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.SparkContext
@@ -12,32 +14,46 @@ import java.io.File
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import sys.process.stringSeqToProcess
-import org.apache.spark.SparkConf 
-import org.apache.spark.rdd.RDD 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.streaming.StreamingContext._
-
+import org.apache.spark.SparkConf
+    /*
+     * argument 0 - path of HDFS file or directory to read
+     * argument 1 - path where file is stored in destination
+     * argument 2 - Gateway Id
+     * argument 3 - Start Date time
+     * argument 4 - End Date time
+     */
+ 
 object HDFSFileReader {
-
+ 
   def main(args: Array[String]) {
     System.setProperty("HADOOP_USER_NAME", "hadoop");
-    println("Trying to read file from HDFS...")
-    val conf = new SparkConf().setMaster("local[*]").setAppName("HDFSFileReader")
-    conf.set("fs.defaultFS", "hdfs://localhost:9000")
-    val sc = new SparkContext(conf)
-    
-   val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-      import sqlContext.implicits._
-    val data = sc.textFile("hdfs://localhost:9000/dummy_storage/ImpalaTesdata/2017/9/18").toDF
-    data.rdd.saveAsTextFile("\\usr\\share\\spark")
-    
- 
-    var gateway =sqlContext.jsonFile("hdfs://localhost:9000/dummy_storage/ImpalaTesdata/2017/9/18/111111252.json")
-    gateway.registerTempTable("gateway")
+    println("Retrive data from HDFS...")
+    var query =""
+    if(args.length > 1){
+      if(args(2) !=null){
+         query = "from.uuid = "+ args(2)
+      }
+      if(args(3) !=null){
+        query =query + " and rxDateTime > "+ args(3)
+      }
+      if(args(4) !=null){
+        query =query + " and rxDateTime < "+ args(4)
+      }
+    }
+   
+    println("Query to fetch data:  "+query)
+    val conf = new org.apache.spark.SparkConf().setMaster("local[*]").setAppName("fdl-hdfs-reader")
+    conf.set("fs.defaultFS", "hdfs://lolz1186.lol.de.bosch.com:9000")
+    val sc = new org.apache.spark.SparkContext(conf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
+    var gateway =sqlContext.read.json(args(0))
     gateway.show()
     gateway.printSchema()
-    val b = sqlContext.sql("select * from gateway")
-    b.collect.foreach(println)
+   
+    if(query.length()!=0){
+     gateway.filter(query)
+    }
+    gateway.rdd.saveAsTextFile(args(1))
   }
 }
